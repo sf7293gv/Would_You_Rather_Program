9145bcc13b6c7dbb98ebafff6ef55550b8dca33a
Sonic <sf7293gv@go.minnstate.edu>
2021-04-15 08:23:27 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="app">
+
+    <h1>Would you rather...</h1>
+
+    <would-you-rather
+        v-bind:questions="questions"
+        v-on:answer-changed="answerChanged"
+        v-on:answer-changed2="answerChanged2"
+        v-on:answer-changed3="answerChanged3">
+
+    </would-you-rather>
+
+    <ul class="ulist">
+        <li v-for="choice in choices">{{ choice }}</li>
+    </ul>
+
+
+  </div>
+</template>
+
+<script>
+import WouldYouRather from './components/WouldYouRather.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WouldYouRather
+  },
+  data() {
+    return {
+      questions: [
+        {
+          id: 0,
+          wyrQuestion: 'Would you rather pick color green or blue?',
+          wyrAnswer1: 'Green',
+          wyrAnswer2: 'Blue',
+        },
+        {
+          id: 1,
+          wyrQuestion: 'Would you rather pick color pink or purple?',
+          wyrAnswer1: 'Pink',
+          wyrAnswer2: 'Purple',
+        },
+        {
+          id: 2,
+          wyrQuestion: 'Would you rather pick color yellow or red?',
+          wyrAnswer1: 'Yellow',
+          wyrAnswer2: 'Red',
+        }
+      ],
+      choices: []
+    }
+  },
+  methods: {
+    answerChanged(choice, choice2, choice3) {
+      console.log(choice)
+      this.choices = [choice, choice2, choice3]
+      this.userSelection = `You chose ${choice}`
+    },
+    // answerChanged2(choice2) {
+    //   console.log(choice2)
+    //   this.userSelection2 = `You chose ${choice2}`
+    // },
+    // answerChanged3(choice3) {
+    //   console.log(choice3)
+    //   this.userSelection3 = `You chose ${choice3}`
+    // }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.ulist {
+  background-color: aqua;
+}
+</style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="wyr">
+
+    <h2>Please make your choice</h2>
+    <div class="q">
+
+    <h3>{{ questions[0].wyrQuestion }}</h3>
+
+    <input type="radio" v-model="choice" v-bind:value="questions[0].wyrAnswer1" v-on:change="choiceMade">
+    <label>{{ questions[0].wyrAnswer1 }}</label>
+
+    <input type="radio" v-model="choice" v-bind:value="questions[0].wyrAnswer2" v-on:change="choiceMade">
+    <label>{{ questions[0].wyrAnswer2 }}</label>
+
+    </div>
+
+    <div class="q">
+
+      <h3>{{ questions[1].wyrQuestion }}</h3>
+
+      <input type="radio" v-model="choice2" v-bind:value="questions[1].wyrAnswer1" v-on:change="choiceMade">
+      <label>{{ questions[1].wyrAnswer1 }}</label>
+
+      <input type="radio" v-model="choice2" v-bind:value="questions[1].wyrAnswer2" v-on:change="choiceMade">
+      <label>{{ questions[1].wyrAnswer2 }}</label>
+
+    </div>
+
+    <div class="q">
+
+      <h3>{{ questions[2].wyrQuestion }}</h3>
+
+      <input type="radio" v-model="choice3" v-bind:value="questions[2].wyrAnswer1" v-on:change="choiceMade">
+      <label>{{ questions[2].wyrAnswer1 }}</label>
+
+      <input type="radio" v-model="choice3" v-bind:value="questions[2].wyrAnswer2" v-on:change="choiceMade">
+      <label>{{ questions[2].wyrAnswer2 }}</label>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    questions: [],
+  },
+  data() {
+    return {
+      choice: '',
+      choice2: '',
+      choice3: ''
+    }
+  },
+  methods: {
+    choiceMade() {
+      this.$emit('answer-changed', this.choice, this. choice2, this.choice3)
+    }
+    // choiceMade2() {
+    //   this.$emit('answer-changed2', this.choice2)
+    // },
+    // choiceMade3() {
+    //   this.$emit('answer-changed3', this.choice3)
+    // }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+.wyr {
+  border: 2px black solid;
+  background: aquamarine;
+}
+
+.q {
+  margin: 20px 0 0;
+  border: 1px black solid;
+  background-color: cadetblue;
+}
+
+</style>
